669f3ddffe56715d497af3c24f6e5734a6cdd76e
Diana <dianapoplacenel9@gmail.com>
2020-06-09 13:01:34 -0700

    deleted unnecessary file

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Team from "./Team";
 import useSticky from "./useSticky"; 
 import Header from "./Header"; 
 import Activities from "./Activities/Activities"; 
-import Classes from "./Classes/Classes";
 import NewTeam from "./NewTeam/NewTeam";
 import NewClasses from "./Classes/NewClasses"; 
 import Menu from "./Menu";  
@@ -25,23 +24,7 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          {/* <Route path="/register">
-            <Navbar/> 
-            <Login
-              title="Register"
-              formURL="http://localhost:3001/register"
-              OAuth="Or Sign Up Using" 
-            />
-          </Route>
-          <Route path="/login">
-            <Navbar/> 
-            <Login 
-              title="Login"
-              formURL="http://localhost:3001/login"
-              forgot="Forgot Password?"
-              OAuth="Or Sign In Using"
-            />
-          </Route> */}
+          
           <Route path="/activities">
             <Navbar/> 
             <Activities/>
