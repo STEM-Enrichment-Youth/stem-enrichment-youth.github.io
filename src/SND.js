c54f9d68a6d00824002dff54568256211358a3c6
Diana <dianapoplacenel9@gmail.com>
2020-08-13 00:16:13 -0700

    added ctv info

--- src/SND.js
+++ src/SND.js
@@ -10,6 +10,7 @@ import chemistry from "./icons/beaker-atom.png"; import biology from "./icons/ge
 import physics from "./icons/Newtons-cradle.png"; 
 import math from "./icons/checkboard-graph.png"; 
 import neuroscience from "./icons/head-brains.png"; 
+import ctv from "./Partners/Logos/carrythevision.png"; 
 
 function SND() {
 
@@ -17,6 +18,37 @@ function SND() {
     <div class="whole-home" style={{marginTop: "70px"}}>
       <div style={{ background: "url(" + { stemey } + ")", backgroundImage: "cover"}}>
         <div class="head-txt" style={{width: "90%", marginLeft: "5%"}}>
+        <div>
+            <div>
+                {/* <div style={{display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "20px"}}>
+                    <h1 style={{textAlign: "center"}} class="biggest-header-2">stemey X SND</h1>
+                    <img class="stemey-logo" style={{width: "85px", height: "auto", marginLeft: "50px", marginRight: "50px"}} src={stemey} />
+                    <img style={{width: "70px", height: "70px"}} src="https://static.wixstatic.com/media/3129a4_603dabd0e2ba41a5b039ea8c576870aa~mv2.png/v1/fill/w_59,h_60,al_c,q_85,usm_0.66_1.00_0.01/Untitled_Artwork_edited.webp"></img>
+                </div> */}
+                <h1 style={{textAlign: "center"}} class="biggest-header-4">Carry the Vision</h1>
+              <p  style={{textAlign: "left", lineHeight: "2", width: "90%", marginLeft: "5%"}}>
+              Carry the Vision restores lives and communities through nonviolence education creating a peaceful and compassionate world.
+              </p>
+              <h2 style={{marginBottom: "20px"}}>Programs</h2>
+              <div class="ctv-stemey">
+              <a style={{color: "white"}} class="btn btn-full" href="http://carrythevision.org/restore-for-kids/">Restore! for Kids</a>
+                <a style={{color: "white"}} class="btn btn-full" href="http://carrythevision.org/restore-training-and-workshops/">Restore! Training and Workshops</a>
+                <a style={{color: "white"}} class="btn btn-full" href="http://carrythevision.org/restore-life-skills-for-service-providers/">Restore! Life Skills for Service Providers</a>
+                <a style={{color: "white"}} class="btn btn-full" href="http://carrythevision.org/restore-for-reentry-and-incarcerated-youth-and-adults/">Restore! for Reentry and Incarcerated Youth and Adult</a>
+                <a style={{color: "white"}} class="btn btn-full" href="http://carrythevision.org/restore-leadership-program-and-train-the-trainer/">Restore Leadership Program and Train the Traine</a>
+                <a style={{color: "white"}} class="btn btn-full" href="http://www.carrythevision.org/">Learn More about CTV</a>
+              </div>
+
+              
+            </div>
+            <div style={{display: "grid", gridTemplateRows: "1fr", justifyItems: "center", gridGap: "20px"}}>
+                <div>
+                    <img style={{width: "300px", marginBottom: "20px"}} src={ctv} />
+                </div>
+            </div>
+            
+           
+          </div>
           <div>
             <div>
                 {/* <div style={{display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "20px"}}>
