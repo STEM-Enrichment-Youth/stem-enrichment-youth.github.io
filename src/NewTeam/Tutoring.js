09f9b154671a2de33f3bda1ae990874bdc7e0c7d
navaneet614 <navaneet@gmail.com>
2021-01-16 22:51:00 -0800

    Update Tutoring.js

--- src/NewTeam/Tutoring.js
+++ src/NewTeam/Tutoring.js
@@ -13,17 +13,17 @@ export default function NewTeam() {
             <div class="con">
 
                 <Card
-                    imgURL={matilda}
-                    name="Matilda Eriksson"
-                    position="Tutoring Co-Director"
-                    des="Matilda Eriksson is a senior at Leigh High School. She is passionate about various STEM related subjects, currently focusing on mathematics, biology and physics. She has completed AP Physics 1 and is currently taking AP biology, Computer science, and AP calculus. She is also a part of California Scholarship Federation, Science National Honors Society and Link Crew. She loves working with kids and works as an assistant teacher for the Swedish School of Silicon Valley where she teaches kids Swedish."
+                    imgURL={casey}
+                    name="Casey Lopez"
+                    position="Tutoring Director"
+                    des="Casey Lopez is a junior at Olympian High School. She loves biology and math, and works to further accessible STEM education for others. She helps run Olympian’s Helyx Initiative non-profit chapter, where she coordinates tutoring, review sessions, and career panels. She is a long-term member of Better Education for Women in Science and Engineering (BE WiSE). She has received recognition for her Rheumatoid Arthritis research with the Scripps Research Institute 2020 SciMET Competition and seeks to further this research in the future. Hoping to enter the medical field one day, Casey is a public health intern on a Sexual Health Advisory Board Advocated By Youth (SHABABY) and volunteers at her local SHARP hospital as a convalescent center aide. Outside of STEM, Casey is a two year co-captain of Olympian Colorguard and Varsity Winterguard, treasurer of Olympian Dance Company, a Link Crew leader, Peer Mediator, School Site Council chairperson, Chula Vista Animal Shelter volunteer, and a part of the Olympian Band Council Student Life Team.                    "
                 />
 
                 <Card
-                    imgURL={casey}
-                    name="Casey Lopez"
-                    position="Tutoring Co-Director"
-                    des="Casey Lopez is a junior at Olympian High School. She loves biology and math, and works to further accessible STEM education for others. She helps run Olympian’s Helyx Initiative non-profit chapter, where she coordinates tutoring, review sessions, and career panels. She is a long-term member of Better Education for Women in Science and Engineering (BE WiSE). She has received recognition for her Rheumatoid Arthritis research with the Scripps Research Institute 2020 SciMET Competition and seeks to further this research in the future. Hoping to enter the medical field one day, Casey is a public health intern on a Sexual Health Advisory Board Advocated By Youth (SHABABY) and volunteers at her local SHARP hospital as a convalescent center aide. Outside of STEM, Casey is a two year co-captain of Olympian Colorguard and Varsity Winterguard, treasurer of Olympian Dance Company, a Link Crew leader, Peer Mediator, School Site Council chairperson, Chula Vista Animal Shelter volunteer, and a part of the Olympian Band Council Student Life Team.                    "
+                    imgURL={matilda}
+                    name="Matilda Eriksson"
+                    position="Tutoring Assistant Director"
+                    des="Matilda Eriksson is a senior at Leigh High School. She is passionate about various STEM related subjects, currently focusing on mathematics, biology and physics. She has completed AP Physics 1 and is currently taking AP biology, Computer science, and AP calculus. She is also a part of California Scholarship Federation, Science National Honors Society and Link Crew. She loves working with kids and works as an assistant teacher for the Swedish School of Silicon Valley where she teaches kids Swedish."
                 />
 
                 <Card
