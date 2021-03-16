94ec69a325e2bd64d89541330ea9792fb2ac272a
dianasty <dianapoplacenel9@gmail.com>
2021-03-16 01:15:39 -0700

    Added STEM World 2 Info/Links

--- src/STEMWorld2.js
+++ src/STEMWorld2.js
@@ -82,9 +82,13 @@ export default class STEMWorld2 extends Component {
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
               <img className="stemey-logo" src={logo} />
             </div>
-            <h2 style={{ textAlign: "center", fontSize: "23px", marginBottom: "30px" }}> A free virtual stem convention </h2>
-            <h2 style={{ textAlign: "center", fontSize: "19px", marginBottom: "25px" }}> When: March 20-28th on Zoom & Discord</h2>
+            <h2 style={{ textAlign: "center", fontSize: "23px", marginBottom: "30px" }}> An unforgettable experience to build your future in STEM.</h2>
+            <h2 style={{ textAlign: "center", fontSize: "19px", marginBottom: "25px" }}> When: March 19-28th on Zoom & Discord</h2>
             <h2 style={{ textAlign: "center", fontSize: "19px", marginBottom: "25px" }}> Who: Everyone is eligible to participate; however, only grades 12 or lower can compete. </h2>
+            <h2 style={{ textAlign: "center", fontSize: "15px", width: "80%", marginLeft: "10%", marginBottom: "25px" }}> Hosted by STEM Enrichment Youth and the Education For All Foundation, STEM World II is a virtual, free STEM convention consisting of professional speaker workshops, high-school level competitions, and networking opportunities. We welcome STEM enthusiasts of ALL backgrounds, ages, ethnicities, genders, sexualities, and cultures.</h2>  
+            <h2 style={{ textAlign: "center", fontSize: "13px", marginBottom: "25px" }}> Full schedule shown below. </h2>
+            <p style={{ textAlign: "center" }}><a class="btn btn-full" href="https://tinyurl.com/stemworld2">RSVP (Closes 03/20)</a></p>
+            <p style={{ textAlign: "center" }}><a class="btn btn-full" href="https://tinyurl.com/stemworld2-i">Informational Document</a></p>
 
 
             <div class="container" style={{ paddingTop: "0" }}>
@@ -94,34 +98,56 @@ export default class STEMWorld2 extends Component {
                     <div>
                       <h2 style={{ textAlign: "left", lineHeight: "2" }}> Speaker Workshops </h2>
                       <p>Attend workshops led by industry professionals and top-ranking university professors from around the world, including researchers at Duke, Dartmouth, MIT, & more! </p>
-                      <p>Topics Include: Financial Literacy, STEM Education in Reducing Global Poverty, Artificial Intelligence, COVID-19 is Changing STEM, COVID-19 Vaccines, and Biological Engineering</p>
+                      <p>Workshop Topics include. . .</p>
+                      <p>A Guide to Financial Literacy and the Importance of Management</p>
+                      <p>Examining the Role of STEM Education in Reducing Global Poverty</p>
+                      <p>Artificial Intelligence in Education </p>
+                      <p>How COVID-19 is Changing STEM and the Jobs of Tomorrow</p>
+                      <p>Analysis of COVID-19 Vaccines and Reactions</p>
+                      <p>Biological Engineering, and the Ethical Controversy behind Genetic Engineering</p>
+
                     </div>
                   </Col>
+                  
                   <Col sm={6}>
-                    <div style={{ textAlign: "left", lineHeight: "2", width: "90%", marginLeft: "5%" }}>
-                      <h2 style={{ textAlign: "left", lineHeight: "2" }}> Competitions </h2>
-                      <p>Individual or a team of 3-4 people</p>
-                      <p>Subjects: Math, Biology, Physics, Chemistry</p>
-
+                    <div>
+                      <h2 style={{ textAlign: "left", lineHeight: "2" }}> Global Networking </h2>
+                      <p> Join the STEM World II sTEAMs to connect with a tight-knit, global community of STEM enthusiasts. Each sTEAM will host a variety of social events throughout STEM World II (03/20-28), including team dinners, game nights, cooking sessions, and more! </p>
+                      <p>Participants will be sorted into 4 houses: Matcha, Oolong, Jasmine, Chai</p>
+                      <p>Each house will have exclusive bonding events: Game Nights, Speed Friending, Boba/Food Chats, Talent Shows</p>
+                      <h2 style={{ textAlign: "left", fontSize: "23px", marginBottom: "30px" }}> Interested in the sTEAMs?</h2>
+                      <p style={{ textAlign: "left" }}><a class="btn btn-full" href="https://tinyurl.com/stemeydiscord">Join the STEMEY Discord</a></p>
+                      <p style={{ textAlign: "left" }}><a class="btn btn-full" href="https://tinyurl.com/steamsquiz">Take our sTEAMs Sorting Quiz</a></p>
                     </div>
                   </Col>
                   <Col sm={6}>
                     <div>
-                      <h2 style={{ textAlign: "left", lineHeight: "2" }}> Global Networking </h2>
-                      <p>Participants will be sorted into 4 houses: Matcha, Oolong, Jasmine, Chai</p>
-                      <p>Each house will have exclusive bonding events: Game Nights, Speed Friending, Boba/Food Chats, Talent Shows</p>
+                      <h2 style={{ textAlign: "left", lineHeight: "2" }}> Competitions </h2>
+                      <p>Interested in testing your STEM knowledge and winning amazing prizes? Compete in STEM World II between 3/20-28. There will be Team and Individual rounds for competitions. All competitions will be conducted virtually and asynchronously over a 1-week long time slot.</p>
+                      <h2 style={{ textAlign: "left", fontSize: "25px", marginBottom: "10px" }}> Individual Competitions </h2>
+          <p>You will compete by yourself in 1-hour long tests, and you can choose which subject you would like to compete in.</p>
+                      <p>There will be separate competitions for Math, Biology, Physics, and/or Chemistry.</p>
+                    
                     </div>
                   </Col>
                   <Col sm={6}>
-                    <div style={{ textAlign: "center", lineHeight: "2", width: "90%", marginLeft: "5%" }}>
-                      <h2 style={{ textAlign: "left", lineHeight: "2" }}> Sponsored By </h2>
+                    <div>
+                    <h2 style={{ textAlign: "left", fontSize: "25px", marginTop: "20px" }}> Team Competitions </h2>
+                      <p style={{textAlign: "left"}}>You will compete in a team of 3-4 participants for two 60-minute long tests with high school level questions. </p>
+                      <p style={{textAlign: "left"}}>If you do not have a team, please request a team on the STEM World II RSVP form, and we will match you with other competitors as soon as possible.</p>
+                      <p style={{textAlign: "left"}}>Each competition will consist of questions from Math, Biology, Physics, AND Chemistry. </p>
+                      <p style={{ textAlign: "left" }}><a class="btn btn-full" href="https://tinyurl.com/stemworld2">Register to Compete</a></p>
+                    </div>
+                  </Col>
+                  <Col sm={6}>
+                    <h2 style={{ textAlign: "left", lineHeight: "2" }}> Sponsored By </h2>
                       <img style={{ width: "40%", padding: "20px" }} src={sponsor1} />
                       <img style={{ width: "40%", padding: "20px" }} src={sponsor2} />
-                    </div>
                   </Col>
                 </Row>
 
               </Container>
+              
             </div>
 
             <h1 class="biggest-header-2" style={{ marginTop: "50px" }}>Schedule</h1>
